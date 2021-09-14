fdb92c0e29173f993a23304d7349e46941425d23
David ALEXANDRE <david.alexandre@w6d.io>
2021-09-14 23:37:42 +0200

    add error method

--- errorx/errorx.go
+++ errorx/errorx.go
@@ -1,68 +1,79 @@
 package errorx
 
 import (
-    "fmt"
-    "github.com/pkg/errors"
+	"fmt"
+	"github.com/pkg/errors"
 )
 
 type Interface interface {
-    // Error type representing an error condition, with the nil value representing no error.
-    Error() string
+	// Error type representing an error condition, with the nil value representing no error.
+	Error() string
 
-    // GetCause type representing an error condition, with the nil value representing no error.
-    GetCause() error
+	// GetCause type representing an error condition, with the nil value representing no error.
+	GetCause() error
 
-    // GetMessage returns the Message
-    GetMessage() string
+	// GetMessage returns the Message
+	GetMessage() string
 
-    // GetStatusCode returns http status code.
-    GetStatusCode() int
+	// GetStatusCode returns http status code.
+	GetStatusCode() int
 
-    // ShowStack prints the stack if exist
-    ShowStack()
+	// ShowStack prints the stack if exist
+	ShowStack()
 }
 
 type Error struct {
-    // Cause original error
-    Cause   error
+	// Cause original error
+	Cause error
 
-    // Code for http response code
-    Code int
+	// Code for http response code
+	Code int
 
-    // Message details for this error
-    Message string
+	// Message details for this error
+	Message string
 }
 
 var _ Interface = &Error{}
 
 func (e *Error) GetMessage() string {
-    return e.Message
+	return e.Message
 }
 
 func (e *Error) GetStatusCode() int {
-    return e.Code
+	return e.Code
 }
 
 func (e *Error) GetCause() error {
-    return errors.Cause(e.Cause)
+	return errors.Cause(e.Cause)
 }
 
 func (e *Error) ShowStack() {
-    type stackTracer interface {
-        StackTrace() errors.StackTrace
-    }
+	type stackTracer interface {
+		StackTrace() errors.StackTrace
+	}
 
-    if err, ok := errors.Cause(e.Cause).(stackTracer); ok {
-        for _, f := range err.StackTrace() {
-            fmt.Printf("%+s:%d\n", f, f)
-        }
-    }
+	if err, ok := errors.Cause(e.Cause).(stackTracer); ok {
+		for _, f := range err.StackTrace() {
+			fmt.Printf("%+s:%d\n", f, f)
+		}
+	}
 }
 
 func (e *Error) Error() string {
-    if e.Cause == nil {
-        return e.Message
-    }
-    return errors.Wrap(e.Cause, e.Message).Error()
+	if e.Cause == nil {
+		return e.Message
+	}
+	return errors.Wrap(e.Cause, e.Message).Error()
 }
 
+func New(err error, message string) error {
+	return &Error{Cause: err, Message: message}
+}
+
+func NewHTTP(err error, code int, message string) error {
+	return &Error{Cause: err, Code: code, Message: message}
+}
+
+func Wrap(err error, message string) error {
+	return &Error{Cause: errors.Wrap(err, message)}
+}
--- errorx/errorx_test.go
+++ errorx/errorx_test.go
@@ -1,30 +1,35 @@
 package errorx_test
 
 import (
-    . "github.com/onsi/ginkgo"
-    . "github.com/onsi/gomega"
-    "github.com/pkg/errors"
-    "github.com/w6d-io/x/errorx"
-    "net/http"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
+	"github.com/w6d-io/x/errorx"
+	"net/http"
 )
 
 var _ = Describe("Error x", func() {
-    Context("Method", func() {
-        It("check", func() {
-            By("build errorx")
-            e := errorx.Error{
-                Cause: errors.New("unit test"),
-                Code: http.StatusInternalServerError,
-                Message: "message error",
-            }
-            Expect(e.Error()).To(Equal(errors.Wrap(errors.New("unit test"), "message error").Error()))
-            Expect(e.GetStatusCode()).To(Equal(http.StatusInternalServerError))
-            Expect(e.GetMessage()).To(Equal("message error"))
-            By("remove cause")
-            e.Cause = nil
-            Expect(e.Error()).To(Equal("message error"))
-            //Expect(e.GetMessage()).To(Equal("message error"))
-
-        })
-    })
+	Context("Method", func() {
+		It("check", func() {
+			var errUnitTest = errors.New("unit test")
+			By("build errorx")
+			e := errorx.Error{
+				Cause:   errors.New("unit test"),
+				Code:    http.StatusInternalServerError,
+				Message: "message error",
+			}
+			Expect(e.Error()).To(Equal(errors.Wrap(errUnitTest, "message error").Error()))
+			Expect(e.GetStatusCode()).To(Equal(http.StatusInternalServerError))
+			Expect(e.GetMessage()).To(Equal("message error"))
+			Expect(e.GetCause().Error()).To(Equal("unit test"))
+			e.ShowStack()
+			By("remove cause")
+			e.Cause = nil
+			Expect(e.Error()).To(Equal("message error"))
+			Expect(errorx.New(nil, "")).To(HaveOccurred())
+			Expect(errorx.NewHTTP(nil, 200, "")).To(HaveOccurred())
+			err := errorx.Wrap(errUnitTest, "error wrapped")
+			Expect(err).To(HaveOccurred())
+		})
+	})
 })
