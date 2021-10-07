72a38fc1fbaa66a0125d9dfd3dcf010f2eeebb6d
David ALEXANDRE <david.alexandre@w6d.io>
2021-10-07 08:43:25 +0200

    feature: add cmdx with version and helper

---
+++ cmdx/helper.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2020 WILDCARD SA.
+
+Licensed under the WILDCARD SA License, Version 1.0 (the "License");
+WILDCARD SA is register in french corporation.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.w6d.io/licenses/LICENSE-1.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is prohibited.
+Created on 07/10/2021
+*/
+
+package cmdx
+
+import (
+    "fmt"
+    "os"
+)
+
+// Must fatales with the optional message if err is not nil.
+func Must(err error, message string, args ...interface{}) {
+    if err == nil {
+        return
+    }
+
+    _, _ = fmt.Fprintf(os.Stderr, message+"\n", args...)
+    os.Exit(1)
+}
---
+++ cmdx/version.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2020 WILDCARD SA.
+
+Licensed under the WILDCARD SA License, Version 1.0 (the "License");
+WILDCARD SA is register in french corporation.
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.w6d.io/licenses/LICENSE-1.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is prohibited.
+Created on 07/10/2021
+*/
+
+package cmdx
+
+import (
+    "fmt"
+    "github.com/spf13/cobra"
+    "os"
+)
+
+// Version returns a *cobra.Command that handles the application version
+func Version(tag, commit, buildTime *string) *cobra.Command {
+    return &cobra.Command{
+        Use: "version",
+        Short: "Show the build version, build commit and build time",
+        Run: func(cmd *cobra.Command, args []string) {
+            if len(*tag) == 0 {
+                _, _ = fmt.Fprintln(os.Stderr, "Unable to determine version because the build process did not properly configure it.")
+            } else {
+                fmt.Printf("Version:			%s\n", *tag)
+            }
+
+            if len(*commit) == 0 {
+                _, _ = fmt.Fprintln(os.Stderr, "Unable to determine commit sha because the build process did not properly configure it.")
+            } else {
+                fmt.Printf("Build Commit:			%s\n", *commit)
+            }
+
+            if len(*buildTime) == 0 {
+                _, _ = fmt.Fprintln(os.Stderr, "Unable to determine build timestamp because the build process did not properly configure it.")
+            } else {
+                fmt.Printf("Build Commit:			%s\n", *buildTime)
+            }
+        },
+    }
+}
