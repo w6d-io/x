555ad475e35978596f32d156557196a71ac04573
David ALEXANDRE <9482408+david972@users.noreply.github.com>
2022-03-22 19:41:34 +0100

    feat: add toolx package (#25)

---
+++ toolx/toolx.go
@@ -0,0 +1,20 @@
+package toolx
+
+import "reflect"
+
+func InArray(val interface{}, array interface{}) (exists bool) {
+    exists = false
+    kind := reflect.TypeOf(array).Kind()
+    switch kind {
+    case reflect.Slice:
+        s := reflect.ValueOf(array)
+
+        for i := 0; i < s.Len(); i++ {
+            if reflect.DeepEqual(val, s.Index(i).Interface()) == true {
+                exists = true
+                return
+            }
+        }
+    }
+    return
+}
